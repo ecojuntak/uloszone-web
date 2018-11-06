611e0df4a3a6ebc6ba1fbc5e568c4ae8b4e48a77
Lily Anatasia Naibaho <if415002@students.del.ac.id>
2018-11-06 20:48:10 +0700

    Search Product by color still in progress

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,8 @@ Route::get('/', function () {
 
 Auth::routes();
 
+Route::get('query', 'SearchController@search');
+
 Route::middleware(['auth'])->group(function(){
     Route::get('/home', 'HomeController@index')->name('home');
     Route::get('/test', 'HomeController@test');
